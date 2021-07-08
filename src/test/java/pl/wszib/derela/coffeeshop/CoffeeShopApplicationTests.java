3e760a45455b02aa57537caab4a9cb050843eb8b
KrisD1977 <derela@student.wszib.edu.pl>
2021-07-08 18:26:59 +0200

    Init

---
+++ src/test/java/pl/wszib/derela/coffeeshop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.derela.coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
